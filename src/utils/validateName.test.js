b315c78652f97a0548b45c437e0521715799dac5
Pablo Labra <tu-correo-unab@ejemplo.com>
2026-01-19 15:05:21 -0300

    Fix: Correcciones 19/01

--- src/utils/validateName.test.js
+++ src/utils/validateName.test.js
@@ -3,6 +3,20 @@ import { filterAllowedValuesForName } from './filterAllowedValuesForName';
 test("Valida pablo o'higgins como verdadero", () => {
     expect(filterAllowedValuesForName("pablo o'higgins")).toBe(true);
 });
+test('Rechazar false si el nombre es un valo vacio',() => {
+    expect(filterAllowedValuesForName("")).toBe(false);
+});
 test('Rechaza nombres cortos', () => {
     expect(filterAllowedValuesForName("Jo")).toBe(false);
+});
+test('Rechazar comillas en nombres que no sean o\'higgins',() => {
+    expect(filterAllowedValuesForName("pablo'")).toBe(false);
+    expect(filterAllowedValuesForName("d'pablo")).toBe(false);
+});
+test('Rechaza nombre que tengan numeros',() => {
+    expect(filterAllowedValuesForName("Pablo123")).toBe(false);
+});
+test('Rechaza nombres con simbolos espeiales prohibidos', () => {
+    expect(filterAllowedValuesForName("Pablo#$%")).toBe(false);
+    expect(filterAllowedValuesForName("Pablo_ohiggins")).toBe(false);
 });
